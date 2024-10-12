af1b17801ae71fde7c4f558221d1e4e1d86229b9
Arshad Khan <108711604+arshadkh507@users.noreply.github.com>
2024-10-12 09:06:08 +0500

    updated oct 2024: design updation, adding little project, etc.

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,1 +1,36 @@
-const body=document.querySelector("body");document.addEventListener("DOMContentLoaded",function(){const burgerMenu=document.getElementById("burger-menu");const menuToggle=document.getElementById("menu-toggle");const navList=document.querySelector(".navbar ul");const burgerIcon=document.getElementById("burger-icon");burgerMenu.addEventListener("click",function(){if(menuToggle.checked){navList.style.right="-300px";burgerIcon.querySelector("i").classList.remove("fa-xmark");burgerIcon.querySelector("i").classList.add("fa-bars")}else{navList.style.right="0";burgerIcon.querySelector("i").classList.remove("fa-bars");burgerIcon.querySelector("i").classList.add("fa-xmark")}});document.querySelectorAll('a[href^="#"]').forEach(anchor=>{anchor.addEventListener('click',function(e){e.preventDefault();document.querySelector(this.getAttribute('href')).scrollIntoView({behavior:'smooth'})})});const tabs=document.querySelectorAll('.about-tabs li');const tabContents=document.querySelectorAll('.tab-content');tabs.forEach((tab,index)=>{tab.addEventListener('click',()=>{tabs.forEach((tab)=>tab.classList.remove('active'));tabContents.forEach((content)=>content.classList.remove('active'));tab.classList.add('active');tabContents[index].classList.add('active')})})})
+const body = document.querySelector("body");
+document.addEventListener("DOMContentLoaded", function () {
+  const burgerMenu = document.getElementById("burger-menu");
+  const menuToggle = document.getElementById("menu-toggle");
+  const navList = document.querySelector(".navbar ul");
+  const burgerIcon = document.getElementById("burger-icon");
+  burgerMenu.addEventListener("click", function () {
+    if (menuToggle.checked) {
+      navList.style.right = "-300px";
+      burgerIcon.querySelector("i").classList.remove("fa-xmark");
+      burgerIcon.querySelector("i").classList.add("fa-bars");
+    } else {
+      navList.style.right = "0";
+      burgerIcon.querySelector("i").classList.remove("fa-bars");
+      burgerIcon.querySelector("i").classList.add("fa-xmark");
+    }
+  });
+  document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+    anchor.addEventListener("click", function (e) {
+      e.preventDefault();
+      document
+        .querySelector(this.getAttribute("href"))
+        .scrollIntoView({ behavior: "smooth" });
+    });
+  });
+  const tabs = document.querySelectorAll(".about-tabs li");
+  const tabContents = document.querySelectorAll(".tab-content");
+  tabs.forEach((tab, index) => {
+    tab.addEventListener("click", () => {
+      tabs.forEach((tab) => tab.classList.remove("active"));
+      tabContents.forEach((content) => content.classList.remove("active"));
+      tab.classList.add("active");
+      tabContents[index].classList.add("active");
+    });
+  });
+});
